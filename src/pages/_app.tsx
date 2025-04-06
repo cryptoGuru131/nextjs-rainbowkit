56224aa17401820b4c3da0c830e2e01b442613c5
blockchaingale <anthonylawson0917@gmail.com>
2025-04-07 06:33:35 +1000

    initialize Next.js project with RainbowKit and wagmi integration; add configuration files, styles, and components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { AppProps } from "next/app";
+import { WagmiProvider  } from "wagmi";
+// import { publicProvider } from "wagmi/providers/public";
+import { ChainProvider, ThirdwebProvider } from "thirdweb/react";
+
+import { config } from '../wagmi';
+
+const client = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiProvider config={config}>
+      <ThirdwebProvider>
+          <QueryClientProvider client={client}>
+            <RainbowKitProvider>
+              <Component {...pageProps} />
+            </RainbowKitProvider> 
+          </QueryClientProvider>
+      </ThirdwebProvider>
+    </WagmiProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/wagmi.ts
@@ -0,0 +1,95 @@
+import { createThirdwebClient, defineChain as thirdwebChain } from 'thirdweb';
+import { createConfig, http } from 'wagmi';
+import { inAppWalletConnector } from "@thirdweb-dev/wagmi-adapter";
+import {
+  arbitrum,
+  goerli,
+  mainnet,
+  optimism,
+  polygon,
+  base,
+  zora,
+  avalanche,
+  bsc,
+  Chain,
+} from 'wagmi/chains';
+
+const client = createThirdwebClient({
+  clientId: "654d245030441fe158c45b853815aa08",
+});
+
+const sonic: Chain = {
+  id: 146,
+  name: "Sonic",
+  nativeCurrency: {
+    decimals: 18,
+    name: "s",
+    symbol: "S",
+  },
+  rpcUrls: {
+    default: { http: ["https://rpc.soniclabs.com"] },
+    public: { http: ["https://rpc.soniclabs.com"] }, // Replace with your RPC URL
+  },
+  blockExplorers: {
+    default: {
+      name: "SonicScan",
+      url: "https://soniscan.org",
+    },
+  },
+}
+
+const berachain: Chain = {
+  id: 80094,
+  name: "Berachain",
+  nativeCurrency: {
+    decimals: 18,
+    name: "bera",
+    symbol: "BERA",
+  },
+  rpcUrls: {
+    default: { http: ["https://rpc.berachain.com"] },
+    public: { http: ["https://rpc.berachain-apis.com"] }, // Replace with your RPC URL
+  },
+  blockExplorers: {
+    default: {
+      name: "Berascan",
+      url: "https://berascan.com",
+    },
+  },
+}
+
+export const config = createConfig({
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    zora,
+    bsc,
+    avalanche,
+    berachain,
+    sonic,
+  ],
+  connectors: [
+    // add the in-app wallet connector
+    inAppWalletConnector({
+      client,
+      smartAccount: {
+        sponsorGas: true,
+        chain: thirdwebChain(1)
+      }
+    }),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+    [polygon.id]: http(),
+    [optimism.id]: http(),
+    [bsc.id]: http(),
+    [berachain.id]: http(),
+    [sonic.id]: http(),
+    [avalanche.id]: http(),
+    [arbitrum.id]: http(),
+  },
+});
