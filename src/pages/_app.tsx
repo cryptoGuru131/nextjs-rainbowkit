a25945d2c4856820f29bf1e4420b19fa823f553b
Crypto_Lover <cryptolover20131@gmail.com>
2025-06-17 17:11:47 +1000

    Update _app.tsx
    Co-authored-by: blockchaingale <anthonylawson0917@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import type { AppProps } from "next/app";
+
 import { WagmiProvider  } from "wagmi";
 import { ChainProvider, ThirdwebProvider } from "thirdweb/react";
 
