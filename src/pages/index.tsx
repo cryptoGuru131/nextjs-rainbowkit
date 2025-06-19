6051f6bebdfe4fc955f2d83cb2dae311c5ffd255
Crypto_Lover <cryptolover20131@gmail.com>
2025-06-20 03:19:43 +1000

    Update index.tsx
    Co-authored-by: blockchaingale <anthonylawson0917@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ const berachain = {
 const chains = [mainnet, polygon, arbitrum, optimism, avalanche, sonic, zora, berachain];
 
 const client = createThirdwebClient({
-  clientId: "654d245030441fe158c45b853815aa08"
+  clientId: ""
 })
 
 const wallets = [
